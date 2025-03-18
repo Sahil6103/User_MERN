3479bfa40fd2c2338877c191c9a03963f4183758
sahil <sahildhandhukiya0@gmail.com>
2025-03-18 21:49:44 +0530

    user

--- Backend/Routes/user.route.js
+++ Backend/Routes/user.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../Controllers/CrudController");
+const { registerUser, loginUser } = require("../Controllers/crudController");
 const router = express.Router();
 
 router.post("/register", registerUser);
