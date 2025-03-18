007942e6ffc5b353f6740cdfb1740d2c0be11e95
sahil <sahildhandhukiya0@gmail.com>
2025-03-18 21:59:42 +0530

    user

--- Frontend/src/services/authServices.js
+++ Frontend/src/services/authServices.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/auth";
-
 export const registerUser = async (formData) => {
   try {
-    const response = await axios.post(`${API_URL}/register`, formData);
+    const response = await axios.post(
+      `${process.env.API_URL}/register`,
+      formData
+    );
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || "Registration Failed!");
@@ -13,7 +14,7 @@ export const registerUser = async (formData) => {
 
 export const loginUser = async (formData) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, formData);
+    const response = await axios.post(`${process.env.API_URL}/login`, formData);
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || "Login Failed!");
