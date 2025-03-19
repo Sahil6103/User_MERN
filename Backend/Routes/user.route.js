441a7dff63ca0c591468d7be638413a99f0c7b20
sahil <sahildhandhukiya0@gmail.com>
2025-03-19 12:05:30 +0530

    done

--- Backend/Routes/user.route.js
+++ Backend/Routes/user.route.js
@@ -3,6 +3,9 @@ const { registerUser, loginUser } = require("../Controllers/crudController");
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.get("/register", (req, res) => {
+  res.send("hii");
+});
 router.post("/login", loginUser);
 
 module.exports = router;
